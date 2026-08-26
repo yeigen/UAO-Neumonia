b8acbea140d2caada2f3b35efde3488a51973481
BStevenRE <brandonenri123@gmail.com>
2026-08-26 00:00:39 -0500

    refactor: move TF env setup to src package init    chore:update comments

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-import os
+import os 
 
-os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "3")
-os.environ.setdefault("TF_ENABLE_ONEDNN_OPTS", "0")
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "3")   #Reducir mensajes de tensorflow en consola.
+os.environ.setdefault("TF_ENABLE_ONEDNN_OPTS", "0")  #Desactivar optimizaciones de oneDNN de TF, para un entorno mas controlado
