7d9b7376d85849029af58a3d7e62f22e9154eb8f
BStevenRE <brandonenri123@gmail.com>
2026-08-26 15:58:54 -0500

    chore:update comments

--- src/preprocess_img.py
+++ src/preprocess_img.py
@@ -4,10 +4,10 @@ import numpy as np
 from src.config import CLAHE_CLIP_LIMIT, CLAHE_TILE_GRID_SIZE, IMG_SIZE
 
 #transformaciones
-def preprocess(array):
+def preprocess(array):   #Función para converitr la imagen al formato esperado por el modelo
     resized = cv2.resize(array, (IMG_SIZE, IMG_SIZE)) # 512x512
-    grayscale = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY) # los canales se ordenan al reves porque asi los carga la libreria
-    clahe = cv2.createCLAHE(clipLimit=CLAHE_CLIP_LIMIT, tileGridSize=CLAHE_TILE_GRID_SIZE) # resaltar la imagen
+    grayscale = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY) # Los canales se ordenan al reves (BGR) porque asi los carga la libreria y los convierte a 1 canal
+    clahe = cv2.createCLAHE(clipLimit=CLAHE_CLIP_LIMIT, tileGridSize=CLAHE_TILE_GRID_SIZE) # Resaltar la imagen, mejorando el contraste
     equalized = clahe.apply(grayscale)
-    normalized = equalized / 255 # escalar a de [0, 255] a [0.0, 1.0]
-    return np.expand_dims(normalized, axis=(0, -1)) # (batch, alto, ancho, canales) (1 [lote], 512, 512, 1 [canales])
+    normalized = equalized / 255 # Escalar de [0, 255] a [0.0, 1.0] 
+    return np.expand_dims(normalized, axis=(0, -1)) # (batch, alto, ancho, canales) (1 [lote], 512, 512, 1 [canales]) - Estrutura para Keras
