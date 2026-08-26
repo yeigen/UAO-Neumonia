d479df1bca76764e63264d42dcb172607380a0fe
BStevenRE <brandonenri123@gmail.com>
2026-08-26 00:19:43 -0500

    chore:comentarios

--- src/load_model.py
+++ src/load_model.py
@@ -4,4 +4,4 @@ from src.config import MODEL_PATH  #Importar ruta del modelo
 
 #Cargar el modelo sin cambiar su configuración
 def model_fun(path=MODEL_PATH):
-    return tf.keras.models.load_model(path, compile=False) # cargamos el modelo sin reentrenar
+    return tf.keras.models.load_model(path, compile=False) # Cargamos el modelo sin reentrenar
