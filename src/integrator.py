252b7a0fa41943cff8f0c7cce7513dc21be41a21
BStevenRE <brandonenri123@gmail.com>
2026-08-26 23:52:51 -0500

    refactor: centralize project constants in config module chore:update comments

--- src/integrator.py
+++ src/integrator.py
@@ -6,11 +6,11 @@ from src.load_model import model_fun
 from src.preprocess_img import preprocess
 
 
-def predict(array, model=None):
+def predict(array, model=None):   #Recibir imagen para realizar predicción
     if model is None:
         model = model_fun()
-    probabilities = model.predict(preprocess(array), verbose=0)[0]
-    label = CLASS_LABELS[int(np.argmax(probabilities))]
+    probabilities = model.predict(preprocess(array), verbose=0)[0]   #Obtiene probabilidades de predicción de cada clase
+    label = CLASS_LABELS[int(np.argmax(probabilities))]    #Obtiene la clase con mayor probabilidad y asigna la etiqueta
     probability = float(probabilities.max()) * 100
-    heatmap = grad_cam(array, model)
-    return label, probability, heatmap
+    heatmap = grad_cam(array, model)      #Genera el mapa gradcam
+    return label, probability, heatmap    #Clase predicha, probabilidad y mapa de calor gradcam
