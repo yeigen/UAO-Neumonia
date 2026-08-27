e6aedd090596b6d0347ecce539d133e1035ffc8d
BStevenRE <brandonenri123@gmail.com>
2026-08-27 00:44:58 -0500

    chore:update comments

--- src/integrator.py
+++ src/integrator.py
@@ -10,7 +10,7 @@ def predict(array, model=None):   #Recibir imagen para realizar predicción
     if model is None:
         model = model_fun()
     probabilities = model.predict(preprocess(array), verbose=0)[0]   #Obtiene probabilidades de predicción de cada clase
-    label = CLASS_LABELS[int(np.argmax(probabilities))]    #Obtiene la clase con mayor probabilidad y asigna la etiqueta
+    label = CLASS_LABELS[int(np.argmax(probabilities))]    #Obtiene la clase con mayor probabilidad y le asigna la etiqueta
     probability = float(probabilities.max()) * 100
     heatmap = grad_cam(array, model)      #Genera el mapa gradcam
     return label, probability, heatmap    #Clase predicha, probabilidad y mapa de calor gradcam
