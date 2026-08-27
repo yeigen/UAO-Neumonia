b51ca3253e1bbe0119820bca4408ff3a140facd0
BStevenRE <brandonenri123@gmail.com>
2026-08-27 00:42:33 -0500

    chore:update comments

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 import os 
 
-os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "3")   #Reducir mensajes de tensorflow en consola.
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "3")   #Reducir mensajes de tensorflow en consola
 os.environ.setdefault("TF_ENABLE_ONEDNN_OPTS", "0")  #Desactivar optimizaciones de oneDNN de TF, para un entorno mas controlado
